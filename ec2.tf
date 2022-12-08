d87684c75e4eefa04d948f01284429bd04735d53
lee0916jh <lee0916jh@gmail.com>
2022-12-07 23:19:53 -0800

    add terraform skeleton

--- ec2.tf
+++ ec2.tf
@@ -14,45 +14,8 @@ resource "aws_default_subnet" "default_az1" {
   }
 }
 
-resource "aws_security_group" "ec2_security_group" {
-  name = "ec2 security group"
-  description = "ec2 security group"
-  vpc_id = aws_default_vpc.default_vpc.id
-
-  ingress {
-    description = "https access"
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
-
-  ingress {
-    description = "http access"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
-
-  ingress {
-    description = "ssh access"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = -1
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
-
-  tags = {
-    Name = "ec2 security group"
-  }
+data "aws_security_group" "ec2_security_group" {
+  id = "sg-0a939df5a2350d9f5"
 }
 
 data "aws_ami" "ubuntu" {
