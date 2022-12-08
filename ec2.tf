e4bdcfdd1dbb6589cdaacf6e465986f5fd5804c4
lee0916jh <lee0916jh@gmail.com>
2022-12-07 23:08:30 -0800

    add terraform skeleton

--- ec2.tf
+++ ec2.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "ec2_security_group" {
   ingress {
     description = "https access"
     from_port = 443
-    to_port = 433
+    to_port = 443
     protocol = "tcp"
     cidr_blocks = [ "0.0.0.0/0" ]
   }
